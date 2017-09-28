a882e80689bde35aff666f31b8d352591ae5da63
Oreo455 <oreo4555@gmail.com>
2017-09-28 19:01:30 -0400

    Changes Joe and Garry Changes

---
+++ Calculator/Main.py
@@ -0,0 +1,14 @@
+def main():
+    print("Welcome to Guess the Human!")
+    gameType = input("Select Game Type: Guess,FunTip, BirthFacts)")
+    input.
+    if (gameType !="Guess" or gameType!="FunTip"):
+        print("Please try again")
+    elif gameType == "Guess":
+     firstquestion = input("Are you black:")
+    if firstquestion == "Yes":
+            print("You are Garry!")
+
+
+
+main()
